8092d766b23d7fb630fc146520d7e8f2e0116392
Hector Archer <hectorae28@gmail.com>
2025-02-06 23:56:37 -0400

    add components shadcn

---
+++ client/src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
+
+export default function Home() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Douglas
+        </span>{" "}
+        Gay.
+      </h1>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+      <Button>Test Button</Button>
+    </div>
+  );
+}
