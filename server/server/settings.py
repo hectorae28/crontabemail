773c62183802abb24d9df1944965535f577c3b9a
Hector Archer <hectorae28@gmail.com>
2025-02-17 17:19:03 -0400

    fix packages

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,8 +76,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware'
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "server.urls"
